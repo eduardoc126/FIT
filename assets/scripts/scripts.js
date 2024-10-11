45b5f6fbd2e14da4aaa251498a6d5ca3c94ab5f7
eduardoc126 <developer.eduardo@outlook.com.br>
2024-10-11 14:09:17 -0300

    update

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -16,11 +16,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //////////////////////////////
 
-
 const hbtn = document.getElementById('menu-btn');
 const submenu = document.getElementById('submenu');
 
 hbtn.addEventListener('click', function() {
   hbtn.classList.toggle('active');
   submenu.classList.toggle('off');
+});
+
+//////////////////////////////
+
+
+document.addEventListener('scroll', () => {
+
+  const header = document.getElementById('header');
+  const sectionAll = document.getElementById('show-header');
+
+  const planosPosition = sectionAll.getBoundingClientRect();
+
+  if (planosPosition.top <= 0 && planosPosition.bottom >= 0) {
+    header.classList.remove('hidden');
+    header.classList.add('visible');
+  } else {
+    header.classList.remove('visible');
+    header.classList.add('hidden');
+  }
 });
