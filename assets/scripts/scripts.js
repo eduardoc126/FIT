ad58766231acff15c6015dcf0719b5518ee12fc5
eduardoc126 <developer.eduardo@outlook.com.br>
2024-10-11 11:51:38 -0300

    update

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,39 +1,3 @@
-var form = document.getElementById("my-form");
-
-async function handleSubmit(event) {
-  event.preventDefault();
-  var status = document.getElementById("my-form-status");
-  var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      'Accept': 'application/json'
-    }
-  }).then(response => {
-    if (response.ok) {
-      status.innerHTML = "Mensagem enviada com sucesso!";
-      status.style.color = 'var(--VerdeP)';
-      form.reset()
-    } else {
-      response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-          status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
-        } else {
-          status.innerHTML = "Não foi possível enviar sua mensagem no momento!";
-          status.style.color = 'red';
-        }
-      });
-    }
-  }).catch(error => {
-    status.innerHTML = "Não foi possível enviar sua mensagem no momento!";
-    status.style.color = 'red';
-  });
-}
-
-form.addEventListener("submit", handleSubmit);
-
-
 //////////////////////////////
 
 
@@ -41,14 +5,25 @@ window.onload = function() {
   document.getElementById("anoAtual").textContent = new Date().getFullYear();
 };
 
-
 //////////////////////////////
-
-
 
 document.addEventListener("DOMContentLoaded", function() {
   AOS.init({
     duration: 1000,
     once: true
   });
+});
+
+//////////////////////////////
+
+const btnbar = document.getElementById('btn-bars');
+const bar1 = document.getElementById('bar1');
+const bar2 = document.getElementById('bar2');
+const bar3 = document.getElementById('bar3');
+
+btnbar.addEventListener('click', function() {
+  
+  bar1.classList.add('transform-45')
+  bar2.style.display = 'none';
+  bar3.classList.add('transform45')
 });
